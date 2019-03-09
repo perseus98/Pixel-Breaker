325be8034784446869ad2df0ddaad18d81d2865e
perseus98 <perseusmhrn@gmail.com>
2019-03-09 11:15:52 +0530

    V 0.1- NO intro/starting, Default gameplay

---
+++ src/pixel_breaker/Pixel_Breaker.java
@@ -0,0 +1,120 @@
+
+package pixel_breaker;
+
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+public class Pixel_Breaker extends Application {
+
+    private static MainFrame mainFrame;
+
+    public static MainFrame getMainFrame() {
+        return mainFrame;
+    }
+    
+    @Override public void start(Stage stage) {
+        Config.initialize();
+        Group root = new Group();
+        mainFrame = new MainFrame(root);
+        stage.setTitle("Pixel Breaker");
+        stage.setResizable(false);
+        stage.setWidth(Config.SCREEN_WIDTH + 2*Config.WINDOW_BORDER);
+        stage.setHeight(Config.SCREEN_HEIGHT+ 2*Config.WINDOW_BORDER + Config.TITLE_BAR_HEIGHT);
+        Scene scene = new Scene(root);
+        scene.setFill(Color.BLACK);
+        stage.setScene(scene);
+        mainFrame.changeState(MainFrame.SPLASH);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+
+    public class MainFrame {
+        // Instance of scene root node
+        private Group root;
+
+        // Instance of splash (if exists)
+        private Splash splash;
+//        private Splash1 splash1;
+
+        // Instance of level (if exists)
+        private Level level;
+
+        // Number of lifes
+        private int lifeCount;
+
+        // Current score
+        private int score;
+
+        private MainFrame(Group root) {
+            this.root = root;
+        }
+
+        public int getState() {
+            return state;
+        }
+
+        public int getScore() {
+            return score;
+        }
+
+        public void setScore(int score) {
+            this.score = score;
+        }
+
+        public int getLifeCount() {
+            return lifeCount;
+        }
+
+        public void increaseLives() {
+            lifeCount = Math.min(lifeCount + 1, Config.MAX_LIVES);
+        }
+
+        public void decreaseLives() {
+            lifeCount--;
+        }
+
+        // Initializes game (lifes, scores etc)
+        public void startGame() {
+            lifeCount = 0;
+            score = 0;
+            changeState(1);
+        }
+
+        // Current state of the game. The next values are available
+        // 0 - Splash
+        public static final int SPLASH = 0;
+        // 1..Level.LEVEL_COUNT - Level
+        private int state = SPLASH;
+
+        public void changeState(int newState) {
+            this.state = newState;
+            if (splash != null) {
+                splash.stop();
+            }
+            if (level != null) {
+                level.stop();
+            }
+            if (state < 1 || state > LevelData.getLevelsCount()) {
+                root.getChildren().remove(level);
+                level = null;
+                splash = new Splash();
+                root.getChildren().add(splash);
+                splash.start();
+            } else {
+                root.getChildren().remove(splash);
+                splash = null;
+                level = new Level(state);
+                root.getChildren().add(level);
+                level.start();
+            }
+        }
+    }
+
+}
+
---
+++ src/pixel_breaker/Utils.java
@@ -0,0 +1,25 @@
+
+package pixel_breaker;
+
+public class Utils {
+
+    private static final java.util.Random RANDOM = new java.util.Random();
+
+    // Returns random integer number from 0 to max - 1
+    public static int random(int max) {
+        return (int) (RANDOM.nextDouble() * max);
+    }
+
+    // Returns sign of the value
+    public static int sign(double n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n > 0) {
+            return 1;
+        } else {
+            return -1;
+        }
+    }
+
+}
